db55a5ee243dd16c49563a9e941d121b33159da4
Nate Bate <nate@harvestmedia.com>
2022-01-28 11:56:20 -0500

    Initial Commit

---
+++ 1-hello-world/main.py
@@ -0,0 +1,11 @@
+############################
+# Title: Hello World
+# Description: The classic way to begin learning a programming language.
+#
+# Date: January 28, 2022
+# Author: Nathan A. Bate
+#
+
+print("\n\n***********************************************")
+print("Hello World!")
+print("***********************************************\n\n")
